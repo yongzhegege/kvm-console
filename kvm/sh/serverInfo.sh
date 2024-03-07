45ec31c08eec11aae6861d7b582bc9b7a7ebde86
yongzhegege <94191@126.com>
2024-03-07 14:29:48 +0800

    修复novnc

--- kvm/sh/serverInfo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-free -m | awk '{if($3>1024 && NR==2){printf "%0.1fGB/%0.1fGB (%.2f%%)\n",$3/1024,$2/1024,$3*100/$2 } else if($3<1024 && $2<1024 && NR ==2){printf "%dMB/%dMB (%.2f%%)\n",$3,$2,$3*100/$2} if($3<1024 && $2>1024 && NR ==2){printf "%dMB/%0.1fGB (%.2f%%)\n",$3,$2/1024,$3*100/$2}}'
-df -h | awk '$NF=="/"{printf "%s/%s (%s)\n", $3,$2,$5}'
-top -bn1 | grep Cpu | awk -F 'id' '{print $1}' | awk -F 'ni' '{print $2}' | awk '{print $2}'
--- kvm/sh/ttyd.sh
+++
@@ -1,5 +0,0 @@
-ttydStatus=$(lsof -i:14377 | grep LISTEN)
-if [ -z "$ttydStatus" ]; then
-pkill -9 ttyd
-ttyd -p 14377 login
-fi
